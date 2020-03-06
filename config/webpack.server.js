6acbc4008398b7a3cd7b5459ea3b7075e4be88f4
styx11 <1654630248@qq.com>
2020-03-06 16:10:39 +0800

    :wrench: update webpack server config

---
+++ config/webpack.server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const baseConfig = require('./webpack.base');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = merge(baseConfig, {
+  name: 'server',
+  target: 'node',
+  entry: {
+    server: path.resolve(__dirname, '../src/server.entry.js'),
+  },
+  output: {
+    libraryTarget: 'commonjs2'
+  },
+
+  // 外置 node 环境依赖
+  externals: nodeExternals({
+    whitelist: /\.css$/
+  }),
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.VUE_ENV': '"server"'
+    }),
+  ]
+})
