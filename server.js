fafd016734652717a2b104ad09bbe56eec10999a
styx11 <1654630248@qq.com>
2020-03-12 20:24:59 +0800

    :sparkles: update dist files server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const Koa = require('koa');
+const path = require('path');
+const send = require('koa-send');
 const Router = require('@koa/router');
 
 // SSR 相关
@@ -7,19 +9,39 @@ const createApp = require('./dist/server.bundle');
 const { createRenderer } = require('vue-server-renderer');
 const clientManifest = require('./dist/vue-ssr-client-manifest.json');
 const template = fs.readFileSync('./src/template/index.html', 'utf8');
-
-// Server 相关
-const server = new Koa();
-const router = new Router();
-
-const context = {
-  title: 'Hello SSR',
-};
-const app = createApp(context);
 const renderer = createRenderer({
   runInNewContext: false,
   clientManifest,
   template
+});
+
+// Server 相关
+const server = new Koa();
+const router = new Router();
+const distRouter = new Router({
+  prefix: '/dist'
+});
+
+// 之后可以加上特定文件的 micro-cache 策略
+distRouter.get('*', async ctx => {
+  const filename = ctx.path.split(/\/dist(?:\/)?/)[1];
+  if (!filename) return ctx.throw(404);
+
+  // koa-send 文档示例有 send(ctx, ctx.path, { root: __dirname + '/public' })
+  // 经过测试 ctx.path 应指请求访问的文件名
+  try {
+    await send(ctx, filename, { root: path.resolve(__dirname, './dist') });
+  } catch (e) {
+    ctx.throw();
+  }
+});
+
+router.get('/manifest.json', async ctx => {
+  try {
+    await send(ctx, path.resolve('./manifest.json'));
+  } catch (e) {
+    ctx.throw(e);
+  }
 });
 
 // Render 相关路由
@@ -29,6 +51,9 @@ router.get('*', async (ctx, next) => {
 });
 
 router.get('*', async ctx => {
+  const context = { title: 'Hello SSR' };
+  const app = createApp(context);
+
   try {
     ctx.state.html = await renderer.renderToString(app, context);
   } catch (e) {
@@ -37,9 +62,12 @@ router.get('*', async ctx => {
   }
 });
 
+// 更特定 (specific) 路由放在前面
+server.use(distRouter.routes());
+server.use(distRouter.allowedMethods());
+
 server.use(router.routes());
 server.use(router.allowedMethods());
-
 
 server.listen(8080, () => {
   console.log('Server running at localhost:8080');
