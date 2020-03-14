829c73d4cd59822aa91cd096eceeda911f58a249
styx11 <1654630248@qq.com>
2020-03-14 15:37:45 +0800

    :sparkles: update server bundle renderer and browser cache

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const LRU = require('lru-cache');
 const Router = require('@koa/router');
 
 // SSR 相关
-const createApp = require('./dist/server.bundle');
-const { createRenderer } = require('vue-server-renderer');
+// server bundle 创建的 renderer 会自动将带有 hash 值文件名的js文件引入到 template 中
+const { createBundleRenderer } = require('vue-server-renderer');
+const serverBundle = require('./dist/vue-ssr-server-bundle.json');
 const clientManifest = require('./dist/vue-ssr-client-manifest.json');
 const template = fs.readFileSync('./src/template/index.html', 'utf8');
-const renderer = createRenderer({
+const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false,
   clientManifest,
   template,
@@ -35,7 +36,8 @@ const microCache = new LRU({
   stale: true //允许过期内容，减少请求峰值
 });
 
-// 之后可以加上特定文件的缓存策略
+// 浏览器最大缓存时间视情况更改
+const maxage = process.env.NODE_ENV === 'production' ? 1000 * 60 : 0;
 distRouter.get('*', async ctx => {
   const filename = ctx.path.split(/\/dist(?:\/)?/)[1];
   if (!filename) return ctx.throw(404);
@@ -43,7 +45,7 @@ distRouter.get('*', async ctx => {
   // koa-send 文档示例有 send(ctx, ctx.path, { root: __dirname + '/public' })
   // 经过测试 ctx.path 应指请求访问的文件名
   try {
-    await send(ctx, filename, { root: path.resolve(__dirname, './dist') });
+    await send(ctx, filename, { root: path.resolve(__dirname, './dist'), maxage });
   } catch (e) {
     ctx.throw();
   }
@@ -51,7 +53,7 @@ distRouter.get('*', async ctx => {
 
 router.get('/manifest.json', async ctx => {
   try {
-    await send(ctx, path.resolve('./manifest.json'));
+    await send(ctx, path.resolve('./manifest.json'), { maxage });
   } catch (e) {
     ctx.throw(e);
   }
@@ -73,9 +75,8 @@ router.get('*', async ctx => {
   }
 
   const context = { title: 'Hello SSR', url: ctx.url };
-  const app = createApp(context);
   try {
-    const html = await renderer.renderToString(app, context);
+    const html = await renderer.renderToString(context);
 
     if (cacheable) microCache.set(ctx.url, html);
     ctx.state.html = html;
