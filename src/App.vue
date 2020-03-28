3ae46d21c4a8f15356ba4ee3e2d463e55881d2ff
styx11 <1654630248@qq.com>
2020-03-28 17:13:46 +0800

    :sparkles: update root App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <!-- ssr下因为mount方法调用时标签已被渲染，所以将app id放在这里-->
   <div id="app">
-    <Hello/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue';
-
 export default {
-  components: {
-    Hello
-  }  
+
 }
 </script>
